ee20dbbf7a7a1ca3b2a4147bf818daaabad66101
Till Gerken <till.gerken@gmail.com>
2024-07-24 15:56:15 +0200

    Expose validator

--- lib/evoleen_fhir_events.dart
+++ lib/evoleen_fhir_events.dart
@@ -11,6 +11,7 @@ export 'src/fhir_message_client/models/fhir_message.dart';
 export 'src/fhir_event_processor/abstract_fhir_event_processor.dart';
 export 'src/fhir_event_processor/azure_event_processor.dart';
 export 'src/validators/abstract_event_validator.dart';
+export 'src/validators/poison_event_validator.dart';
 export 'src/fhir_event_processor/abstract_action_executor.dart';
 export 'src/fhir_event_processor/abstract_post_processor.dart';
 export 'src/post_processors/remove_message_post_processor.dart';
